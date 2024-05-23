8139f88b0ba9e441c648e9133529a62ee0028f5b
Sumit Lahiri <lahiri.phdworks@gmail.com>
2024-05-24 03:53:15 +0530

    https://github.com/nevillegrech/gigahorse-toolchain/issues/137
    https://github.com/nevillegrech/gigahorse-toolchain/issues/137

---
+++ runner.sh
@@ -0,0 +1,9 @@
+./solcbin/solc5 --bin-runtime samples/Add.sol -o samples  --overwrite
+mv ./samples/Add.bin-runtime ./samples/Add.hex 
+rm -rf ./samples/Add.bin-runtime
+rm -rf ./cachestore/Add 
+python3 gigahorse.py -C ./clients/visualizeout.py \
+    -w cachestore \
+    -r example.json \
+    -j 10 \
+    ./samples;
---
+++ samples/Add.sol
@@ -0,0 +1,30 @@
+pragma solidity >=0.4.2 <=0.7.6;
+
+contract Add {
+    function one(uint a) 
+        public 
+        pure 
+    {
+       uint c = 0;
+       uint b = 512;
+       while (a > 0 && c < 4096) {
+            a = a - 32;
+            c = c + 4 * b;
+       }
+    }
+
+    function two(uint a) 
+        public 
+        pure 
+    {
+       uint c = 0;
+       uint b = 512;
+       if (a > 0) {
+            a = a + 1024;
+       } else {
+            a = a - 1024;
+       }
+       c = a + b;
+    }
+
+}
